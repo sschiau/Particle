df796eaefcb38fabc4d43464f414f2156f012b83
tradzero <admin@drakframe.com>
2017-10-13 10:20:47 +0800

    reset file format

--- Sources/Particle.php
+++ Sources/Particle.php
@@ -22,62 +22,60 @@
 
 namespace Schiau\Utilities;
 
-abstract class Particle
-{
-    const EPOCH = 1479533469598;
-    const MAX12BIT = 4095;
-    const MAX41BIT = 1099511627775;
+abstract class Particle {
+	const EPOCH = 1479533469598;
+	const max12bit = 4095;
+	const max41bit = 1099511627775;
 
-    protected static $machineId = null;
+	static $machineId = null;
 
-    public static function machineId($mId)
-    {
-        self::$machineId = $mId;
-    }
+	public static function machineId($mId) {
+	    self::$machineId = $mId;
+	}
 
-    public static function generateParticle()
-    {
-        /*
+	public static function generateParticle() {
+		/*
 		* Time - 42 bits
 		*/
-        $time = floor(microtime(true) * 1000);
+		$time = floor(microtime(true) * 1000);
 
-        /*
+		/*
 		* Substract custom epoch from current time
 		*/
-        $time -= self::EPOCH;
+		$time -= self::EPOCH;
 
-        /*
+		/*
 		* Create a base and add time to it
 		*/
-        $base = decbin(self::MAX41BIT + $time);
+		$base = decbin(self::max41bit + $time);
 
-        /*
+		/*
 		* Configured machine id - 10 bits - up to 1024 machines
 		*/
-        $machineid = str_pad(decbin(self::$machineId), 10, "0", STR_PAD_LEFT);
+		$machineid = str_pad(decbin(self::$machineId), 10, "0", STR_PAD_LEFT);
 
-        /*
+		/*
 		* sequence number - 12 bits - up to 4096 random numbers per machine
 		*/
-        $random = str_pad(decbin(mt_rand(0, self::MAX12BIT)), 12, "0", STR_PAD_LEFT);
+		$random = str_pad(decbin(mt_rand(0, self::max12bit)), 12, "0", STR_PAD_LEFT);
 
-        /*
+		/*
 		* Pack
 		*/
-        $base = $base.$machineid.$random;
+		$base = $base.$machineid.$random;
 
-        /*
+		/*
 		* Return unique time id no
 		*/
-        return bindec($base);
-    }
+		return bindec($base);
+	}
 
-    public static function timeFromParticle($particle)
-    {
-        /*
+	public static function timeFromParticle($particle) {
+		/*
 		* Return time
 		*/
-        return bindec(substr(decbin($particle), 0, 41)) - self::MAX41BIT + self::EPOCH;
-    }
+		return bindec(substr(decbin($particle),0,41)) - self::max41bit + self::EPOCH;
+	}
 }
+
+?>
